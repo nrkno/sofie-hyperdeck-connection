695acbbe35dc6546af7fdc6a843fd20a0ac5f1d1
Julian Waller <julian@superfly.tv>
2018-10-11 17:17:26 +0100

    chore: Fill out status and videoformat enums from manual

--- src/enums.ts
+++ src/enums.ts
@@ -1,7 +1,12 @@
 export enum TransportStatus {
 	Preview = 'preview',
+	Stopped = 'stopped',
+	Play = 'play',
+	Forward = 'forward',
+	Rewind = 'rewind',
+	Jog = 'jog',
+	Shuttle = 'shuttle',
 	Record = 'record'
-	// TODO
 }
 
 export enum SlotId {
@@ -10,6 +15,27 @@ export enum SlotId {
 }
 
 export enum VideoFormat {
-	_1080i50 = '1080i50'
-	// TODO
+	NTSC = 'NTSC',
+	PAL = 'PAL',
+	NTSCp = 'NTSCp',
+	PALp = 'PALp',
+	_720p50 = '720p50',
+	_720p5994 = '720p5994',
+	_720p60 = '720p60',
+	_1080p23976 = '1080p23976',
+	_1080p24 = '1080p24',
+	_1080p25 = '1080p25',
+	_1080p2997 = '1080p2997',
+	_1080p30 = '1080p30',
+	_1080i50 = '1080i50',
+	_1080i5994 = '1080i5994',
+	_1080i60 = '1080i60',
+	_4Kp23976 = '4Kp23976',
+	_4Kp24 = '4Kp24',
+	_4Kp25 = '4Kp25',
+	_4Kp2997 = '4Kp2997',
+	_4Kp30 = '4Kp30',
+	_4Kp50 = '4Kp50',
+	_4Kp5994 = '4Kp5994',
+	_4Kp60 = '4Kp60'
 }
