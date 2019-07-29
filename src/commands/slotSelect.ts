c70c50985639a3f26c1207840054f02bf8a0e650
Balte de Wit <balte.dewit@gmail.com>
2019-07-29 16:11:04 +0200

    feat: slot select command

---
+++ src/commands/slotSelect.ts
@@ -0,0 +1,19 @@
+import { AbstractCommandNoResponse } from './abstractCommand'
+import { NamedMessage } from '../message'
+
+export class SlotSelectCommand extends AbstractCommandNoResponse {
+	slotId?: string
+
+	serialize () {
+		const res: NamedMessage = {
+			name: 'slot select',
+			params: {}
+		}
+
+		if (this.slotId) {
+			res.params['slot id'] = this.slotId
+		}
+
+		return res
+	}
+}
