bc0504557bb200d22074b9963d335013b32fa4e6
Balte de Wit <balte.dewit@gmail.com>
2019-09-21 09:46:50 +0200

    feat: goto command

---
+++ src/commands/goTo.ts
@@ -0,0 +1,35 @@
+import { NamedMessage } from '../message'
+import { AbstractCommandNoResponse } from './abstractCommand'
+
+export class GoToCommand extends AbstractCommandNoResponse {
+    clip?: string
+    clipId?: number
+    timecode?: string
+
+	constructor (clip?: string, clipId?: number, timecode?: string) {
+		super()
+
+        this.clip = clip
+        this.clipId = clipId
+        this.timecode = timecode
+	}
+
+	serialize () {
+		const res: NamedMessage = {
+			name: 'goto',
+			params: {}
+        }
+
+        if (this.clip !== undefined) res.params.clip = this.clip
+        if (this.clipId !== undefined) res.params.clipId = this.clipId + ''
+        if (this.timecode !== undefined) res.params.timecode = this.timecode
+
+        if (Object.keys(res.params).length === 0) {
+            throw new Error('GoToCommand needs at least one parameter')
+        } else if (Object.keys(res.params).length > 1) {
+            throw new Error('GoToCommand should have at most one parameter')
+        }
+
+		return res
+	}
+}
