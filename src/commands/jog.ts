cbcd0cf7e803ec56cf0054c654c9d2875218534f
Balte de Wit <balte.dewit@gmail.com>
2019-09-21 09:47:18 +0200

    feat: jog command

---
+++ src/commands/jog.ts
@@ -0,0 +1,25 @@
+import { NamedMessage } from '../message'
+import { AbstractCommandNoResponse } from './abstractCommand'
+
+export class JogCommand extends AbstractCommandNoResponse {
+    timecode?: string
+
+	constructor (timecode?: string) {
+		super()
+
+        this.timecode = timecode
+	}
+
+	serialize () {
+		const res: NamedMessage = {
+			name: 'jog',
+			params: {}
+        }
+
+        if (this.timecode === undefined) throw new Error('JogCommand needs timecode')
+
+        res.params.timecode = this.timecode
+
+		return res
+	}
+}
